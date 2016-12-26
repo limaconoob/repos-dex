be9bc001a7843926e883512e32738b3abbfd8d32
Jérémy PEPIN <jpepin@student.42.fr>
2016-12-27 00:31:15 +0100

    Update example

--- example.c
+++ example.c
@@ -6,7 +6,7 @@
 /*   By: jpepin <jpepin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/27 00:14:45 by jpepin            #+#    #+#             */
-/*   Updated: 2016/12/27 00:23:56 by jpepin           ###   ########.fr       */
+/*   Updated: 2016/12/27 00:30:32 by jpepin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,13 @@
 #include <stdlib.h>
 #include <strings.h>
 
-char (*(ghoster)(void))[4096]
-{ static char the[4096];
-  bzero(the, 4096);
-  return (&the); }
-
 void start(void *cur_dir)
-{ static char (*the)[4096];
-  the = ghoster();
-  getcwd(*the, 4096);
-  *((unsigned char **)cur_dir) = *the; }
+{ char the[4096];
+  bzero(the, 4096);
+  getcwd(the, 4096);
+  *((char **)cur_dir) = the; }
 
 int main (void)
-{ static unsigned char *tmp[1];
+{ unsigned char *tmp[1];
   start(tmp);
   printf("TMP::%s\n", *tmp); }
